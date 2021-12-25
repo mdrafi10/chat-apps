508869e3b02aae9e2d773d1922ade786ea57f687
MD RAFI <79692008+mdrafi10@users.noreply.github.com>
2021-12-25 18:53:31 +0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -266,3 +266,13 @@ EditPhoto.addEventListener("click", () => {
   showP.style.transition = "0.4s";
 });
 
+window.addEventListener("resize", () => {
+  const chats = document.querySelector(".chatApp");
+  if (window.innerHeight < 670 && window.innerWidth > 640) {
+    chats.style.paddingTop = "20px";
+  } else if (window.innerWidth < 640) {
+    chats.style.paddingTop = "0px";
+  } else {
+    chats.style.paddingTop = "8rem";
+  }
+});
